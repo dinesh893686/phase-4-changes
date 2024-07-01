7376d6beba857821af0cd012fc558b2613b655b4
dinesh893686 <dineshshah8234@gmail.com>
2024-07-01 13:15:53 +0530

    code-refactor

--- src/main/java/com/example/demo/repository/PlayerPerformanceRepository.java
+++ src/main/java/com/example/demo/repository/PlayerPerformanceRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface PlayerPerformanceRepository extends JpaRepository<PlayerPerformance, Long> {
-    Optional<PlayerPerformance> findByMatchIdAndPlayerId(Long matchId, Long playerId);
 }
